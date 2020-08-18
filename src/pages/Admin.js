a4ff331cacbff7cf0f4b55b24dd5a1ecbafd3761
Adit Modi <adit.modi24@gmail.com>
2020-08-18 11:39:28 +0530

    Update Admin.js

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'aaa
+import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import { API, graphqlOperation, Storage } from "aws-amplify";
 import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
